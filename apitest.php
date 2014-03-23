ccdf9ce6a01607014ae18542903bc540eb6c1e63
John Schavey <jschavey@me.com>
2014-03-22 19:40:26 -0600

    API Test

---
+++ apitest.php
@@ -0,0 +1,15 @@
+Testing New API....
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+<script>
+$( document ).ready( function() {
+	$( "#results" ).load( "https://data.colorado.gov/resource/cdhe-degrees-awarded.json?programName=Computer%20Science" );
+	
+});
+</script>
+
+<div id="results">
+</div>
+
+End of Page.
