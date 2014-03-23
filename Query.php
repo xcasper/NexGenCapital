b62dc62d29ee4ad9d24ec1b59240444624a23abd
John Schavey <jschavey@me.com>
2014-03-23 09:34:14 -0600

    Query Prototyping

--- Query.php
+++ Query.php
@@ -12,37 +12,37 @@
 //	Debug	***********************************************************************************
 
 $debug = 1;
-if( $debug ) echo '<br />$debug => '.$debug.'<br />';
 
 if( $debug ) {
+	echo '<br />$debug => '.$debug.'<br />';
+
 	error_reporting(E_ALL);
 	ini_set("display_errors", 1);
 }
 
-$_REQUEST['agemax']				= 17;
-$_REQUEST['institutionname']	= NULL;
-$_REQUEST['gender']				= "Female";
-$_REQUEST['studentlevel']		= NULL;
-$_REQUEST['programname']		= "Computer Science";
-
 //	Inputs	***********************************************************************************
 
-$agemax				= $_REQUEST['agemax'];
+if( isset( $_REQUEST['agemax'] ) ) $agemax = $_REQUEST['agemax'];
 if( $debug ) echo '<br />$agemax => '.$agemax.'<br />';
 
-$institutionname	= $_REQUEST['institutionname'];
+if( isset( $_REQUEST['institutionname'] ) ) $institutionname	= $_REQUEST['institutionname'];
 if( $debug ) echo '<br />$institutionname => '.$institutionname.'<br />';
 
-$gender				= $_REQUEST['gender'];
+if( isset( $_REQUEST['gender'] ) ) $gender	= $_REQUEST['gender'];
 if( $debug ) echo '<br />$gender => '.$gender.'<br />';
 
-$studentlevel		= $_REQUEST['studentlevel'];
+if( isset( $_REQUEST['studentlevel'] ) ) $studentlevel = $_REQUEST['studentlevel'];
 if( $debug ) echo '<br />$studentlevel => '.$studentlevel.'<br />';
 
-$programname		= $_REQUEST['programname'];
+if( isset( $_REQUEST['prgramname'] ) ) $programname	= $_REQUEST['programname'];
 if( $debug ) echo '<br />$programname => '.$programname.'<br />';
 
-unset( $agemax, $institutionname, $gender, $studentlevel, $programname);
+$agemax				= 17;
+$institutionname	= NULL;
+$gender				= "Female";
+$studentlevel		= NULL;
+$programname		= "Computer Science";
+
 
 $inputs				= [
 	"agemax" 			=> $agemax,
@@ -50,16 +50,28 @@ $inputs				= [
 	"studentlevel"		=> $studentlevel,
 	"programname"		=> $programname
 ];
+if( $debug ) echo '<br />$inputs => '.print_r( $inputs ).'<br />';
+
+unset( $agemax, $institutionname, $gender, $studentlevel, $programname );
+
+$keys		= array();
+$values		= array();
 
 foreach( $inputs as $key => $value ) {
 	if( $debug ) echo '<br />$key => '.$key.' $value => '.$value.'<br />';
-	if( empty( $value ) ) unset( $key );
-	else {
-		//	Unzip $key => $value
+	if( empty( $value ) ) {
+		if( $debug ) echo '<br />Unset $key => '.$key.'<br />';
+		unset( $inputs[$key] );	
+	} else {
+		if( $debug ) echo '<br />Else loop:<br />';		
 		$keys[]	 	= $key;
-		$values[]	= $key;
-} unset( $key, $value );
-
+		if( $debug ) echo '<br />$keys => '.print_r( $keys ).'<br />';
+		$values[]	= $value;
+		if( $debug ) echo '<br />$values => '.print_r( $values ).'<br />';
+		
+	}
+}
+unset( $key, $value );
 
 //	Build dat Query	***************************************************************************
 
@@ -67,25 +79,49 @@ $select	= "SELECT ";
 
 for( $i = 1; $i < count( $keys ); $i++ ) {
 if( $debug ) echo '<br />$i => '.$i.'<br />';
+if( $debug ) echo '<br />count( $keys ) => '.count( $keys ).'<br />';
 	
-	switch( count( $inputs ) {
+	switch( count( $inputs ) ) {
 		case 0:
+			if( $debug ) echo '<br />Case 0: <br />';
 			$select	.= '* ';
 			break;
 		case 1:
-			$select	.= $keys	
-			$where	.= "where ".$keys."='".$values."'";
+			if( $debug ) echo '<br />Case 1: <br />';
+			$select	.= $keys;
+			$where	= "where ".$keys[$i]."='".$values[$i]."'";
 			break;
 		default: 
-			foreach( $keys as $key ) {
-				$select	.= $keys	
-				$where	.= "where ".$keys."='".$values."' AND ";
+			if( $debug ) echo '<br />Case Default: <br />';
+			
+			$where = "WHERE ";
+			
+			foreach( $inputs as $key => $value ) {
+				if( $debug ) echo '<br />$key => '.$key.'<br />';
+				$select	.= $key.', ';	
+				$where	.= $key."='".$value."' AND ";
 			}
+			
+			$where	= trim( $where, 'AND ' );
+			
 			break;
 	}
 	
-	$query	.= $select.'FROM #q8jr-gbau ';
-	if( $debug ) echo '<br />$query => '.$query.'<br />';
 }
 
+//	Strip unnecessary content	***************************************************************
+
+if( $debug ) echo '<br />Strip extraneous strings...<br />';
+$select = trim( $select, ', ' );
+if( $debug ) echo '<br />$select => '.$select.'<br />';
+
+$query	= $select.' FROM #q8jr-gbau ';
+
+if( isset( $where ) ) $query.= $where;
+if( $debug ) echo '<br />$query => '.$query.'<br />';
+
+
 //	Sanitize Inputs ***************************************************************************
+
+/*
+?>
