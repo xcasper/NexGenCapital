6305f8947ad242c7edc0b0cc6c36c712a8601459
John Schavey <jschavey@me.com>
2014-03-24 18:10:39 -0600

    JSON Testing

--- jsontest.php
+++ jsontest.php
@@ -3,6 +3,7 @@ JSON Test Starting...
 <?
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
+ini_set("memory_limit", "768M" );
 
 $json_data = file_get_contents('rows.json');
 json_decode($json_data, true, 1);
